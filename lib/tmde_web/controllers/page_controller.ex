fdcc37458663d5d9260c640e0f03730a79a56b8e
Thorsten Deinert <thorsten.deinert@udo.edu>
2022-08-04 16:33:23 +0200

    Embed parts of README.md on compile time and show them on index page

--- lib/tmde_web/controllers/page_controller.ex
+++ lib/tmde_web/controllers/page_controller.ex
@@ -1,7 +1,20 @@
 defmodule TmdeWeb.PageController do
+  @moduledoc """
+  Controller for homepage (index) and different pages that are required on a website, like imprint and privacy policy.
+  """
   use TmdeWeb, :controller
+  alias Tmde.Helper.Markdown
 
+  # Include part of repo's README as external resource and convert it into html on compile time
+  @external_resource Path.expand("README.md")
+
+  @readme_content Markdown.file_to_html(Path.expand("README.md"),
+                    splitter: "INDEX",
+                    footnotes: true
+                  )
+
+  @doc "Homepage"
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", readme_content: @readme_content)
   end
 end
