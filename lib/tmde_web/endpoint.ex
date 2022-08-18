4f3e137e37b1d205f652f3a8c6503b604ff44bec
Thorsten Deinert <thorsten.deinert@udo.edu>
2022-08-18 15:19:56 +0200

    Serve sitemap.xml via Phoenix

--- lib/tmde_web/endpoint.ex
+++ lib/tmde_web/endpoint.ex
@@ -20,7 +20,7 @@ defmodule TmdeWeb.Endpoint do
     at: "/",
     from: :tmde,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: ~w(assets fonts images favicon.ico robots.txt sitemap.xml)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
