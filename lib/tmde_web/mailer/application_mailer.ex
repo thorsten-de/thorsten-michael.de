4b3d88ceea67bfe0c9a8c06d081bd9b29f8bb387
Thorsten Deinert <thorsten.deinert@udo.edu>
2022-08-31 17:01:49 +0200

    Fleshed out Contacts context to be used for applications

--- lib/tmde_web/mailer/application_mailer.ex
+++ lib/tmde_web/mailer/application_mailer.ex
@@ -1,10 +1,11 @@
 defmodule TmdeWeb.ApplicationMailer do
   use Phoenix.Swoosh, view: TmdeWeb.ApplicationMailerView
+  alias Tmde.Contacts.Contact
 
   def send_application(application) do
     new()
-    |> from({application.sender.name, application.sender.email})
-    |> to({application.contact.name, application.recipient})
+    |> from({Contact.name(application.sender), application.sender.email})
+    |> to({Contact.name(application.contact), application.contact.email})
     |> subject(application.subject)
     |> render_body(:application, application: application, styling: %{bg_color: "#ffffff"})
   end
--- lib/tmde_web/views/application_mailer_view.ex
+++ lib/tmde_web/views/application_mailer_view.ex
@@ -1,25 +1,8 @@
 defmodule TmdeWeb.ApplicationMailerView do
   use TmdeWeb, :view
   import TmdeWeb.Components.MailerComponents
-  alias Tmde.Contact.Link
-
-  def greeting(%{locale: locale, contact: %{gender: "w", name: name}}) when is_binary(name) do
-    Gettext.with_locale(locale, fn ->
-      gettext("Dear Mrs. %{name},", name: name)
-    end)
-  end
-
-  def greeting(%{locale: locale, contact: %{gender: "m", name: name}}) when is_binary(name) do
-    Gettext.with_locale(locale, fn ->
-      gettext("Dear Mr. %{name},", name: name)
-    end)
-  end
-
-  def greeting(%{locale: locale}) do
-    Gettext.with_locale(locale, fn ->
-      gettext("Dear Sirs and Madams,")
-    end)
-  end
+  alias Tmde.Contacts.Link
+  import Tmde.Contacts.Contact, only: [greeting: 1]
 
   def ending(%{locale: locale}) do
     Gettext.with_locale(locale, fn ->
