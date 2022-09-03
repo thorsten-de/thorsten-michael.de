67024847b3c8785a450c2f8a9efbe575283cdedc
Thorsten Deinert <thorsten.deinert@udo.edu>
2022-09-03 19:40:42 +0200

    Turn footer into a functional component

--- lib/tmde_web/views/layout_view.ex
+++ lib/tmde_web/views/layout_view.ex
@@ -5,6 +5,5 @@ defmodule TmdeWeb.LayoutView do
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
 
-  use Bulma
-  import Bulma.Footer
+  alias TmdeWeb.Components.Navigation
 end
