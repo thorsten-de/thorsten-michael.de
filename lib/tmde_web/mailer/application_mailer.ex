7809be7466c2ad265507d7067f9b22cfec227d87
Thorsten Deinert <thorsten.deinert@udo.edu>
2022-09-06 15:55:28 +0200

    Add attachment list and link to application page to mail

--- lib/tmde_web/mailer/application_mailer.ex
+++ lib/tmde_web/mailer/application_mailer.ex
@@ -1,12 +1,32 @@
 defmodule TmdeWeb.ApplicationMailer do
   use Phoenix.Swoosh, view: TmdeWeb.ApplicationMailerView
   alias Tmde.Contacts.Contact
+  import TmdeWeb.Gettext
 
   def send_application(application) do
+    attachments = prepare_attachments(application)
+
     new()
     |> from({Contact.name(application.sender), application.sender.email})
     |> to({Contact.name(application.contact), application.contact.email})
     |> subject(application.subject)
-    |> render_body(:application, application: application, styling: %{bg_color: "#ffffff"})
+    |> render_body(:application,
+      application: application,
+      attachments: attachments,
+      styling: %{bg_color: "#ffffff"}
+    )
+  end
+
+  def prepare_attachments(application) do
+    [
+      %{
+        name: gettext("application documents"),
+        filename:
+          gettext("%{sender} - application documents for %{reference}",
+            sender: application.sender,
+            reference: application.reference
+          )
+      }
+    ]
   end
 end
