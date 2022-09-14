f94373f189f3fd2012dcbc1a0e517f58f12dc1ce
Thorsten Deinert <thorsten.deinert@udo.edu>
2022-09-14 14:59:55 +0200

    Creating a JobSeeker with my Skills for myself

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -12,87 +12,130 @@
 
 alias Tmde.Repo
 alias Tmde.Jobs
-alias Tmde.Content.Translation
+alias Tmde.Jobs
+alias Jobs.Skill
+alias Jobs.PersonalSkill, as: MySkill
 
-translation_mapper = fn obj, field ->
-  Map.update(
-    obj,
-    field,
-    [],
-    Translation.translations(&1)
-  )
-end
+t = fn list -> list |> Tmde.Content.Translation.translations() end
 
-skill_mapper = fn
-  %{} = skill ->
-    translation_mapper.(skill, :label)
-
-  {name, translations} ->
-    %{
-      name: name,
-      label: Enum.map(translations, translation_mapper)
-    }
-
-  name ->
-    %{name: name}
-end
-
-my_skillset =
+my_skillsets =
   [
-    {"C#", %{rating: 1}},
-    {"SQL", %{rating: 1}},
-    {"Elixir", %{rating: 1}},
-    {"Phoenix", %{rating: 2}},
-    {"GraphQL", %{rating: 2}},
-    {"Ruby", %{rating: 2}},
-    {"Rails", %{rating: 2}},
-    {"REST", %{rating: 3}},
-    {"JS", %{rating: 2}},
-    {"HTML", %{rating: 2}},
-    {"CSS", %{rating: 2}},
-    {"Elm", %{rating: 3}},
-    {"PHP", %{rating: 3}},
-    {"Shopware", %{rating: 3}},
-    {"Vue", %{rating: 3}},
-    {"Docker", %{rating: 3}},
-    {"Linux", %{rating: 2}},
-    {%{type: :language, name: "Deutsch", label: [de: "Deutsch", en: "German"]},
-     %{rating: 1, rating_text: [de: "Muttersprache", en: "native speaker"]}},
-    {%{type: :language, name: "English", label: [de: "Englisch", en: "English"]},
-     %{rating: 3, rating_text: [de: "B2", en: "B2 (upper-intermediate)"]}}
+    languages: [
+      %{
+        skill: %{
+          type: :language,
+          name: "Deutsch",
+          label: t.(de: "Deutsch", en: "German")
+        },
+        rating: 100,
+        rating_text: t.(de: "Muttersprache", en: "native speaker")
+      },
+      %{
+        skill: %{
+          type: :language,
+          name: "English",
+          label: t.(de: "Englisch", en: "English")
+        },
+        rating: 70,
+        rating_text: t.(de: "B2", en: "B2")
+      }
+    ],
+    featured: [
+      %{
+        skill: %{
+          name: "C# Application Development",
+          icon: "brands/windows",
+          label:
+            t.(
+              de: "C#/.NET Anwendungen",
+              en: "C#/.NET Applications"
+            )
+        },
+        rating: 84
+      },
+      %{
+        skill: %{
+          name: "SQL Databases",
+          icon: "database",
+          label:
+            t.(
+              de: "SQL / Datenbanken",
+              en: "SQL / Databases"
+            )
+        },
+        rating: 87
+      },
+      %{
+        skill: %{
+          name: "Elixir/Phoenix",
+          icon: "brands/phoenix-framework",
+          label: t.(de: "Elixir / Phoenix")
+        },
+        rating: 80
+      },
+      %{
+        skill: %{
+          name: "Ruby on Rails",
+          icon: "gem",
+          label:
+            t.(
+              de: "Ruby / Rails",
+              en: "Ruby on Rails"
+            )
+        },
+        rating: 75
+      },
+      %{
+        skill: %{
+          name: "PHP/Shopware",
+          icon: "brands/shopware",
+          label:
+            t.(
+              de: "PHP / Shopware",
+              en: "PHP / Shopware"
+            )
+        },
+        rating: 55
+      }
+    ]
   ]
-  |> Enum.map(fn {skill, rating} ->
-    {:ok, skill} =
-      skill
-      |> skill_mapper.()
-      |> Jobs.create_skill()
-
-    {skill, rating}
+  |> Enum.flat_map(fn {category, skills} ->
+    skills
+    |> Enum.with_index()
+    |> Enum.map(fn {item, idx} ->
+      Map.merge(item, %{category: category, sort_order: 1000 + idx * 100})
+    end)
+  end)
+  |> Enum.map(fn %{skill: skill} = my_skill ->
+    {:ok, skill} = Jobs.create_skill(skill)
+    Map.put(my_skill, :skill_id, skill.id)
   end)
 
-{:ok, myself} =
-  Jobs.create_job_seeker(%{
-    contact: %{
-      title: "Dipl.-Inf.",
-      first_name: "Thorsten-Michael",
-      last_name: "Deinert",
-      gender: :male,
-      email: "postmaster@thorsten-michael.de",
-      address: %{street: "Mustergasse 1", zip: "55555", city: "Hauptstadt"}
-    },
-    links: [
-      %{type: :phone, target: "+49 0001 0002 0003"},
-      %{type: :mobile, target: "+49 0001 0002 0003"},
-      %{type: :whatsapp, target: "+49 0001 0002 0003"},
-      %{type: :website, target: "thorsten-michael.de"},
-      %{type: :email, target: "postmaster@thorsten-michael.de"}
-    ],
-    skills:
-      my_skillset
-      |> Enum.map(fn {skill, rating} ->
-        rating
-        |> translation_mapper.(:rating_text)
-        |> Map.merge(%{skill_id: skill.id})
-      end)
-      |> IO.inspect(label: "Skillset")
-  })
+case Jobs.create_job_seeker(%{
+       contact: %{
+         title: "Dipl.-Inf.",
+         first_name: "Thorsten-Michael",
+         last_name: "Deinert",
+         gender: :male,
+         email: "postmaster@thorsten-michael.de",
+         address: %{street: "Mustergasse 1", zip: "55555", city: "Hauptstadt"}
+       },
+       dob: ~D[2000-02-29],
+       place_of_birth: "Irgendwo",
+       marital_status: :married,
+       citizenship: "Europe",
+       links: [
+         %{type: :phone, target: "+49 0001 0002 0003"},
+         %{type: :mobile, target: "+49 0001 0002 0003"},
+         %{type: :whatsapp, target: "+49 0001 0002 0003"},
+         %{type: :website, target: "thorsten-michael.de"},
+         %{type: :email, target: "postmaster@thorsten-michael.de"}
+       ],
+       skills: my_skillsets
+     }) do
+  {:ok, myself} ->
+    IO.inspect(myself)
+
+  {:error, changeset} ->
+    IO.inspect(Ecto.Changeset.traverse_errors(changeset, fn {msg, _} -> msg end))
+end
