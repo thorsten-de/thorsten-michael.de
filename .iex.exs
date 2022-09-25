9dbc43fec40069e5353424314d61168d34d325bb
Thorsten Deinert <thorsten.deinert@udo.edu>
2022-09-25 13:11:11 +0200

    Include chromium in docker container to generate documents

--- .iex.exs
+++ .iex.exs
@@ -28,5 +28,7 @@ end
 
 alias Tmde.Repo
 alias Tmde.Jobs
+# "b207f8f8-662d-4155-baa9-70b82bbd0972"
 application_id = "4655266c-ac42-4b5c-9fb4-21a8dafcd4a0"
 a = Jobs.get_application!(application_id)
+TmdeWeb.DocumentView.generate_documents(a)
