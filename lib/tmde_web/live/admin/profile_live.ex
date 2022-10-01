c1fa8a1b4a7a3784315a87ea1fa8b71e95843950
Thorsten Deinert <thorsten.deinert@udo.edu>
2022-10-01 18:19:16 +0200

    Profile for authenticated job seeker

---
+++ lib/tmde_web/live/admin/profile_live.ex
@@ -0,0 +1,48 @@
+defmodule TmdeWeb.Admin.ProfileLive do
+  use TmdeWeb, :live_view
+  use Bulma
+  alias Tmde.Jobs
+  import TmdeWeb.Components.Forms.JobSeeker
+
+  on_mount TmdeWeb.UserLiveAuth
+
+  def mount(_params, _session, socket) do
+    job_seeker = socket.assigns.current_user
+    changeset = Jobs.change_job_seeker(job_seeker)
+
+    applications = Jobs.job_seeker_applications(job_seeker)
+
+    socket =
+      assign(socket,
+        changeset: changeset,
+        applications: applications
+      )
+
+    {:ok, socket}
+  end
+
+  def handle_event("validate", %{"job_seeker" => params}, socket) do
+    changeset =
+      socket.assigns.current_user
+      |> Jobs.change_job_seeker(params)
+      |> Map.put(:action, :update)
+
+    {:noreply, assign(socket, changeset: changeset)}
+  end
+
+  def handle_event("update_user", %{"job_seeker" => params}, socket) do
+    socket.assigns.current_user
+    |> Jobs.update_job_seeker(params)
+    |> IO.inspect()
+    |> case do
+      {:ok, job_seeker} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "Daten geändert")
+         |> assign(changeset: Jobs.change_job_seeker(job_seeker), current_user: job_seeker)}
+
+      {:error, changeset} ->
+        {:noreply, assign(socket, changeset: changeset)}
+    end
+  end
+end
