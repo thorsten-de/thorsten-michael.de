d62eb5edc42bd15315ddf395181d963ca82e7bbe
Thorsten Deinert <thorsten.deinert@udo.edu>
2022-10-05 10:19:21 +0200

    render_dl not used anymore

--- lib/tmde_web/views/page_view.ex
+++ lib/tmde_web/views/page_view.ex
@@ -2,15 +2,4 @@ defmodule TmdeWeb.PageView do
   use TmdeWeb, :view
   use Bulma
   import TmdeWeb.Components.List, only: [property_list: 1]
-
-  def render_dl(list) do
-    content_tag(:dl, class: "property-list") do
-      for {key, value} <- list do
-        [
-          content_tag(:dt, key),
-          content_tag(:dd, value)
-        ]
-      end
-    end
-  end
 end
