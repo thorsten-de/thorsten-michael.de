6b2b592c4fcbd09b6b04599d144174dd36fa79ae
Thorsten Deinert <thorsten.deinert@udo.edu>
2022-10-05 14:27:58 +0200

    EditorCard switches between presentation and editor - .editor_card functional component to be used inside a live_component - live component EditorCard that toggles itself.

---
+++ lib/tmde_web/components/forms/editor_card.ex
@@ -0,0 +1,17 @@
+defmodule TmdeWeb.Components.Forms.EditorCard do
+  use TmdeWeb, :live_component
+  use Bulma
+  import TmdeWeb.Components.Forms, only: [editor_card: 1]
+
+  def mount(socket) do
+    {:ok, assign(socket, edit?: false)}
+  end
+
+  def handle_event("toggle-editor", _params, socket) do
+    {:noreply, update(socket, :edit?, &(!&1))}
+  end
+
+  def close_editor(id) do
+    send_update(__MODULE__, id: id, edit?: false)
+  end
+end
--- lib/tmde_web/components/forms/forms.ex
+++ lib/tmde_web/components/forms/forms.ex
@@ -15,4 +15,32 @@ defmodule TmdeWeb.Components.Forms do
   def contact_form(assigns) do
     render("contact_form.html", assigns)
   end
+
+  def editor_card(assigns) do
+    assigns =
+      assigns
+      |> assign_defaults(inner_block: [], editor: [], header: nil, edit?: false, target: nil)
+      |> assign_class([])
+
+    ~H"""
+    <.card class={@class}>
+      <:header label={@header}>
+        <a href="#" class="card-header-icon" phx-click="toggle-editor" phx-target={@target}>
+          <.label icon="edit" />
+        </a>
+      </:header>
+      <:footer>
+        <%= if @edit? do %>
+          <a class="card-footer-item" phx-click="toggle-editor" phx-target={@target}><%= gettext("Cancel") %></a>
+          <input class="card-footer-item" type="submit" value={gettext("Save")}>
+        <% end %>
+      </:footer>
+      <%= if @edit? do %>
+        <%= render_slot(@editor) %>
+      <% else %>
+        <%= render_slot(@inner_block) %>
+      <% end %>
+    </.card>
+    """
+  end
 end
