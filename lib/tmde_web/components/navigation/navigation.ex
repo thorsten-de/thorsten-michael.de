100dc8618b101be6210d4c52f55f79d0b772449c
Thorsten Deinert <thorsten.deinert@udo.edu>
2022-10-19 14:15:10 +0200

    Use attr

--- lib/tmde_web/components/navigation/navigation.ex
+++ lib/tmde_web/components/navigation/navigation.ex
@@ -6,7 +6,9 @@ defmodule TmdeWeb.Components.Navigation do
   alias Bulma.Footer
   alias Bulma.Navbar
 
+  attr :conn, :any, default: TmdeWeb.Endpoint
   def footer(assigns), do: render("footer.html", assigns)
 
+  attr :current_user, :any, default: nil
   def navbar(assigns), do: render("navbar.html", assigns)
 end
