0c9f1961e2f361a70bd516d53fc2f056cc898214
Thorsten Deinert <thorsten.deinert@udo.edu>
2022-10-25 11:22:53 +0200

    add @doc for navigation functional components

--- lib/tmde_web/components/navigation/navigation.ex
+++ lib/tmde_web/components/navigation/navigation.ex
@@ -6,9 +6,15 @@ defmodule TmdeWeb.Components.Navigation do
   alias Bulma.Footer
   alias Bulma.Navbar
 
+  @doc """
+  The footer for my website
+  """
   attr :conn, :any, default: TmdeWeb.Endpoint
   def footer(assigns), do: render("footer.html", assigns)
 
+  @doc """
+  The navbar for my website
+  """
   attr :current_user, :any, default: nil
   def navbar(assigns), do: render("navbar.html", assigns)
 end
