5a57a8f5846e6a2051020a82c9ec880280da6599
Thorsten Deinert <thorsten.deinert@udo.edu>
2022-11-24 16:59:00 +0100

    Configure .formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,6 @@
 [
   import_deps: [:ecto, :phoenix, :live_view],
-  inputs: ["*.{ex,exs, heex}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs,heex}"],
+  inputs: ["*.{ex,exs,heex}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs,heex}"],
   subdirectories: ["priv/*/migrations"],
   plugins: [Phoenix.LiveView.HTMLFormatter]
 ]
